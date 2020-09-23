bb024f7aa57c87bd8157722d8641fcaf0e10cf89
JoseLuisDavid19 <Joseluisyoda@gmail.com>
2020-09-22 23:13:44 -0600

    Services

---
+++ Hotel.Rates.Services/Interfaces/IReservationService.cs
@@ -0,0 +1,12 @@
+﻿
+using Hotel.Rates.Core;
+using Hotel.Rates.Data;
+
+namespace Hotel.Rates.Services.Interface
+{
+    public interface IReservationService
+    {
+            ServiceResult<NightlyRatePlan> MakeReservationNighlyPlan(ReservationDataTransferObject reservation);
+
+    }
+}
